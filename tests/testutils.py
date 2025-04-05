ca3de57847282f480405ebcbd32b16a083c3e15c
Antoine Beauchamp <end2endzone@gmail.com>
2025-04-05 11:02:41 -0400

    Created more utility functions for tests.

--- tests/testutils.py
+++ tests/testutils.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 
 def get_project_root_dir_path():
@@ -22,3 +23,46 @@ def get_test_files_dir_path():
     dir_path = os.path.join(dir_path,"tests")
     dir_path = os.path.join(dir_path,"test_files")
     return dir_path
+
+
+def get_test_output_dir_path():
+    dir_path = get_project_root_dir_path()
+    if (dir_path is None):
+        return None
+    dir_path = os.path.join(dir_path,"tests")
+    dir_path = os.path.join(dir_path,"output")
+    return dir_path
+
+
+def run_mkv4cafr(additional_args: list):
+    # Builds args for starting mkv4cafr
+    args = list()
+    args.append("python")
+    args.append("-m")
+    args.append("mkv4cafr.mkv4cafr")
+
+    if (additional_args != None):
+        args.extend(additional_args)
+
+    cwd = get_project_root_dir_path()
+    output_str = None
+
+    try:
+        output_bytes = subprocess.check_output(args, cwd=cwd, shell=True)
+        output_str = output_bytes.decode("utf-8")
+    except subprocess.CalledProcessError as p:
+        output_str = p.output.decode("utf-8")
+
+        err_desc = str()
+        err_desc += "Failed to execute command:\n"
+        err_desc += " ".join(args)
+        err_desc += "\n"
+        err_desc += "Return code: {0}\n".format(p.returncode)
+        err_desc += "Program output:\n"
+        err_desc += output_str
+        err_desc += "\n"
+
+        #raise Exception(err_desc)
+        print(err_desc)
+        return p.returncode
+    return 0
