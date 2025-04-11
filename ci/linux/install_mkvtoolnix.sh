5f2bef2e043071109568ad2a26353e1c626544a4
Antoine Beauchamp <end2endzone@gmail.com>
2025-04-11 13:02:20 -0400

    Added debugging steps in file `ci/linux/install_mkvtoolnix.sh`.

--- ci/linux/install_mkvtoolnix.sh
+++ ci/linux/install_mkvtoolnix.sh
@@ -14,8 +14,11 @@ sudo apt update
 
 # Installing via AppImage as per instructions on https://mkvtoolnix.download/downloads.html#appimage
 export MKVTOOLNIX_DOWNLOAD_VERSION=91.0
+echo "Downloading MKVToolNix_GUI-$MKVTOOLNIX_DOWNLOAD_VERSION-x86_64.AppImage"
 curl -o /tmp/MKVToolNix_GUI-$MKVTOOLNIX_DOWNLOAD_VERSION-x86_64.AppImage https://mkvtoolnix.download/appimage/MKVToolNix_GUI-$MKVTOOLNIX_DOWNLOAD_VERSION-x86_64.AppImage
 chmod u+rx /tmp/MKVToolNix_GUI-$MKVTOOLNIX_DOWNLOAD_VERSION-x86_64.AppImage
+echo done.
+echo
 
 # According to documentation, creating a symlink would allow to run the executables inside the image but that does not look like that.
 #chmod u+rx /tmp/MKVToolNix_GUI-$MKVTOOLNIX_DOWNLOAD_VERSION-x86_64.AppImage
@@ -35,19 +38,30 @@ chmod u+rx /tmp/MKVToolNix_GUI-$MKVTOOLNIX_DOWNLOAD_VERSION-x86_64.AppImage
 #This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.
 #```
 
-# Extract the content of the image instead
+echo "Extracting the content of the image..."
 export MKVTOOLNIX_APP_DIR=/tmp/mkvtoolnix
 pushd /tmp && /tmp/MKVToolNix_GUI-$MKVTOOLNIX_DOWNLOAD_VERSION-x86_64.AppImage --appimage-extract && mv /tmp/squashfs-root $MKVTOOLNIX_APP_DIR && popd
+echo done.
+echo
+
+echo "Setting file exec permissions..."
 chmod u+rx $MKVTOOLNIX_APP_DIR/usr/bin/mkvmerge
 chmod u+rx $MKVTOOLNIX_APP_DIR/usr/bin/mkvpropedit
 sudo ln -s $MKVTOOLNIX_APP_DIR/usr/bin/mkvmerge    /usr/bin/mkvmerge
 sudo ln -s $MKVTOOLNIX_APP_DIR/usr/bin/mkvpropedit /usr/bin/mkvpropedit
+echo done.
+echo
 
 # Add $MKVTOOLNIX_APP_DIR/usr/lib as a shared libraries directory
 # https://unix.stackexchange.com/questions/425251/using-ldconfig-and-ld-so-conf-versus-ld-library-path
 #echo "$MKVTOOLNIX_APP_DIR/usr/lib">/etc/ld.so.conf.d/mkvtoolnix.conf
+echo "Adding directory \'$MKVTOOLNIX_APP_DIR/usr/lib\' as a shared library source directory..."
 echo "$MKVTOOLNIX_APP_DIR/usr/lib" | sudo tee --append /etc/ld.so.conf.d/x86_64-linux-gnu.conf
+echo done.
+echo "Updaing system with ldconfig..."
 ldconfig
+echo done.
+echo
 
 echo mkvtoolnix was installed on the system:
 mkvmerge --version
