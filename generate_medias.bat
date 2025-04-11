4fb3e2627785a8d5f4e08a0ea072d37bc0529c26
Antoine Beauchamp <end2endzone@gmail.com>
2025-04-11 14:52:20 -0400

    Simplified generate_medias.bat

--- generate_medias.bat
+++ generate_medias.bat
@@ -3,6 +3,5 @@ set PATH=%PATH%;C:\Program Files\MKVToolNix
 
 cd /d "%~dp0"
 
-set PYTHONPATH=%CD%;%PYTHONPATH%
-python tools/generate_medias/generate_medias.py
+python -m tools.generate_medias.generate_medias
 pause
