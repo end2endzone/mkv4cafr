3abea48fb100c0fb93f1a5d76f93a35d3515afdb
Antoine Beauchamp <end2endzone@gmail.com>
2025-04-16 22:33:41 -0400

    Added a unit test for testing function is_absolute_file_in_path().

--- tests/mkv4cafrlib/test_findutils.py
+++ tests/mkv4cafrlib/test_findutils.py
@@ -4,7 +4,7 @@ from mkv4cafrlib import findutils
 
 def test_find_exec_in_path():
     # test not found
-    location = findutils.find_exec_in_path("foobar")
+    location = findutils.find_exec_in_path("i-do-not-exists")
     assert location is None
 
     # search for python exec
@@ -12,8 +12,6 @@ def test_find_exec_in_path():
     assert location != None
 
     
-
-
 def test_find_file_in_hints():
     search_paths  = os.curdir
     search_paths += os.pathsep + os.path.join(os.curdir,"medias")
@@ -26,3 +24,32 @@ def test_find_file_in_hints():
     # test to search for an actual media
     location = findutils.find_file_in_hints("test01.mkv", hints)
     assert location != None
+
+
+def test_is_absolute_file_in_path():
+    file_dir = ""
+    file_name = ""
+    bad_dir = ""
+    if os.name == 'nt' or os.name == 'win32': 
+        file_dir = 'C:\\Windows\\system32'
+        file_name = 'cmd.exe'
+        bad_dir = 'c:\\idonotexist'
+    else: 
+        file_dir = '/usr/bin'
+        file_name = 'sh'
+        bad_dir = '/idonotexist'
+
+    # assert failing to find something 
+    file_path = os.path.join(bad_dir, "filename-that-do-not-exists")
+    found = findutils.is_absolute_file_in_path(file_path)
+    assert (found == False)
+
+    # assert finding something 
+    file_path = os.path.join(file_dir, file_name)
+    found = findutils.is_absolute_file_in_path(file_path)
+    assert (found == True)
+
+    # file_name will be found in PATH but not as the 
+    bad_file_path = os.path.join(bad_dir, file_name)
+    found = findutils.is_absolute_file_in_path(bad_file_path)
+    assert (found == False)
